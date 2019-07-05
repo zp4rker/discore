642f99080c3a25e64bea6fb2945e60effc9e9a4d
zaeem <acc@zp4rker.com>
2019-07-05 10:03:48 +1000

    added addrawreaction extension function

--- src/main/kotlin/com/zp4rker/core/discord/util/ExtensionMethods.kt
+++ src/main/kotlin/com/zp4rker/core/discord/util/ExtensionMethods.kt
@@ -1,14 +1,23 @@
 package com.zp4rker.core.discord.util
 
+import net.dv8tion.jda.api.JDA
+import net.dv8tion.jda.api.entities.Emote
+import net.dv8tion.jda.api.entities.Message
 import net.dv8tion.jda.api.entities.MessageChannel
-import net.dv8tion.jda.api.requests.restaction.MessageAction
 
-fun MessageChannel.sendRawString(string: String): MessageAction {
+fun MessageChannel.sendRawString(string: String) = sendMessage(convertEmotes(string, jda))
+
+fun Message.addRawReaction(string: String) = convertEmote(string, jda)?.let { addReaction(it) }
+
+private fun convertEmotes(string: String, jda: JDA): String {
     var content = string
     val regex = Regex(":[^:]*:")
     regex.findAll(string).forEach {
-        val name = it.value.replace(":", "")
-        jda.getEmotesByName(name, false).first()?.let { e -> content = content.replace(it.value, e.asMention) }
+        convertEmote(it.value, jda)?.let { e -> content = content.replace(it.value, e.asMention) }
     }
-    return this.sendMessage(content)
+    return content
+}
+
+private fun convertEmote(string: String, jda: JDA): Emote? {
+    return jda.getEmotesByName(string.replace(":", ""), false).first()
 }
