0747d75afc6027db99d01936a12db8be80c10726
zp4rker <me@zp4rker.com>
2019-08-07 13:20:47 +1000

    a few optimisations

--- src/main/kotlin/com/zp4rker/core/discord/config/Config.kt
+++ src/main/kotlin/com/zp4rker/core/discord/config/Config.kt
@@ -9,4 +9,14 @@ class Config(content: String, private val file: File): JSONObject(content) {
         file.writeText(this.toString(2))
     }
 
+    companion object {
+        fun load(filename: String, sameDir: Boolean = false): Config {
+            val dir = File(Config::class.java.protectionDomain.codeSource.location.toURI()).parentFile
+            val file = if (sameDir) File(dir, filename) else File(filename)
+            val content = file.readText()
+
+            return if (content.isEmpty()) Config("{}", file) else Config(content, file)
+        }
+    }
+
 }
