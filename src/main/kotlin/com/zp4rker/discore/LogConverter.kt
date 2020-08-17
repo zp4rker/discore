97a0d3ec7374b678d831626de549e18573d41ad7
zp4rker <acc@zp4rker.com>
2020-08-17 21:11:30 +1000

    implemented logging

---
+++ src/main/kotlin/com/zp4rker/discore/LogConverter.kt
@@ -0,0 +1,16 @@
+package com.zp4rker.discore
+
+import ch.qos.logback.classic.pattern.MessageConverter
+import ch.qos.logback.classic.spi.ILoggingEvent
+
+class LogConverter : MessageConverter() {
+
+    override fun convert(event: ILoggingEvent): String {
+        return if (event.loggerName == "Discore") {
+            super.convert(event)
+        } else {
+            "[${event.loggerName}] ${super.convert(event)}"
+        }
+    }
+
+}
---
+++ src/main/kotlin/com/zp4rker/discore/Main.kt
@@ -0,0 +1,17 @@
+package com.zp4rker.discore
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+val logger: Logger = LoggerFactory.getLogger("Discore")
+
+class Main {
+
+    companion object {
+        @JvmStatic fun main(args: Array<String>) {
+            logger.info("this is the first line")
+        }
+    }
+
+}
+
