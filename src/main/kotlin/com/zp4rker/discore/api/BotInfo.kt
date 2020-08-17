79f7bec16f632a4a65edbc316c7a795d59ef6229
zp4rker <acc@zp4rker.com>
2020-08-17 14:00:56 +1000

    downgraded some dependencies to stable versions
    went from logback alpha to latest release

--- src/main/kotlin/com/zp4rker/discore/api/BotInfo.kt
+++ src/main/kotlin/com/zp4rker/discore/api/BotInfo.kt
@@ -2,41 +2,49 @@ package com.zp4rker.discore.api
 
 import com.zp4rker.discore.config.JsonFile
 import org.slf4j.LoggerFactory
+import java.io.File
 import kotlin.system.exitProcess
 
 class BotInfo {
 
-    private val data: JsonFile = JsonFile(Bot::class.java.getResource("/bot.json").readText())
+    private var data: JsonFile? = null
 
     private val main: String
-        get() = data.optString("main", "notset")
+        get() = data?.optString("main", "notset") ?: "notset"
 
     val name: String
-        get() = data.optString("name", "notset")
+        get() = data?.optString("name", "notset") ?: "notset"
 
     val version: String
-        get() = data.optString("version", "notset")
+        get() = data?.optString("version", "notset") ?: "notset"
 
     val jdaVersion: String
-        get() = data.optString("jda-version", "notset")
+        get() = data?.optString("jda-version", "notset") ?: "4.2.0_192"
 
     val author: String
-        get() = data.optString("author", "notset")
+        get() = data?.optString("author", "notset") ?: "notset"
 
     val modules: List<String>
-        get() = data.optJSONArray("modules")?.map { it as String }?.toList() ?: emptyList()
+        get() = data?.optJSONArray("modules")?.map { it as String }?.toList() ?: emptyList()
 
-    private fun isValid(): Boolean = arrayOf("name", "version", "main", "author", "jda-version").all(data::has)
+    private fun isValid(): Boolean = arrayOf("name", "version", "main", "author", "jda-version").all(data!!::has)
 
     fun setup() {
-        val logger = LoggerFactory.getLogger("BotLoader")
+        val logger = LoggerFactory.getLogger("Discore")
+
+        Bot::class.java.getResource("/bot.json")?.let { data = JsonFile.load(File(it.toURI())) } ?: kotlin.run {
+            logger.error("bot.json missing! Please create a valid bot.json.")
+            logger.warn("Shutting down...")
+            exitProcess(0)
+        }
 
         if (!isValid()) {
-            logger.error("bot.json invalid! Shutting down...")
+            logger.error("bot.json invalid! Please use a valid bot.json.")
+            logger.error("Shutting down...")
             exitProcess(0)
         } else {
             try {
-                val inst = Class.forName(main).newInstance() as Bot
+                val inst = Class.forName(main).getDeclaredConstructor().newInstance() as Bot
                 inst.run()
             } catch (e: ClassNotFoundException) {
                 logger.error("Unabled to find main class for $name v$version! Shutting down...")
--- src/main/kotlin/com/zp4rker/discore/api/BotLoader.java
+++ src/main/kotlin/com/zp4rker/discore/api/BotLoader.java
@@ -9,35 +9,53 @@ public class BotLoader {
     private static BotInfo bot;
 
     public static void main(String[] args) {
-        System.out.println("Loading libraries, please wait...");
-        loadLibraries();
+        System.out.println("Fetching libraries...");
+        fetchLibraries();
+
+        fetchKotlin();
+        fetchJDA();
 
         bot.setup();
     }
 
-    private static void loadLibraries() {
+    private static void fetchKotlin() {
         //setup kotlin
         List<String> kotlinLibs = Arrays.asList("kotlin-stdlib", "kotlin-stdlib-jdk8");
         kotlinLibs.forEach(it -> {
-            File lib = Library.getFile(it + "-1.3.70.jar");
+            File lib = Library.getFile(it + "-1.3.72.jar");
             if (!lib.exists()) {
-                lib = Library.downloadFromCentral("org.jetbrains.kotlin", it, "1.3.70");
+                lib = Library.downloadFromCentral("org.jetbrains.kotlin", it, "1.3.72");
             }
             Library.add(lib);
         });
+    }
 
+    private static void fetchJDA() {
+        // setup jda
+        bot = new BotInfo();
+        File jdaLib = Library.getFile("JDA-" + bot.getJdaVersion() + ".jar");
+        if (!jdaLib.exists()) {
+            String build = bot.getJdaVersion().split("_")[1];
+            String url = "https://ci.dv8tion.net/job/JDA/" + build + "/artifact/build/libs/JDA-" + bot.getJdaVersion() + "-withDependencies-no-opus.jar";
+            jdaLib = Library.download(url, jdaLib.getName());
+        }
+        assert jdaLib != null;
+        Library.add(jdaLib);
+    }
+
+    private static void fetchLibraries() {
         // setup logback
-        File slfLib = Library.getFile("slf4j-api-2.0.0-alpha1.jar");
+        File slfLib = Library.getFile("slf4j-api-1.7.30.jar");
         if (!slfLib.exists()) {
-            slfLib = Library.downloadFromCentral("org.slf4j", "slf4j-api", "2.0.0-alpha1");
+            slfLib = Library.downloadFromCentral("org.slf4j", "slf4j-api", "1.7.30");
         }
-        File logbackCore = Library.getFile("logback-core-1.3.0-alpha5.jar");
+        File logbackCore = Library.getFile("logback-core-1.2.3.jar");
         if (!logbackCore.exists()) {
-            logbackCore = Library.downloadFromCentral("ch.qos.logback", "logback-core", "1.3.0-alpha5");
+            logbackCore = Library.downloadFromCentral("ch.qos.logback", "logback-core", "1.2.3");
         }
-        File logbackLib = Library.getFile("logback-classic-1.3.0-alpha5.jar");
+        File logbackLib = Library.getFile("logback-classic-1.2.3.jar");
         if (!logbackLib.exists()) {
-            logbackLib = Library.downloadFromCentral("ch.qos.logback", "logback-classic", "1.3.0-alpha5");
+            logbackLib = Library.downloadFromCentral("ch.qos.logback", "logback-classic", "1.2.3");
         }
         Library.addAll(slfLib, logbackCore, logbackLib);
 
@@ -47,17 +65,6 @@ public class BotLoader {
             jsonLib = Library.downloadFromCentral("org.json", "json", "20190722");
         }
         Library.add(jsonLib);
-
-        // setup jda
-        bot = new BotInfo();
-        File jdaLib = Library.getFile("JDA-${info.jdaVersion}.jar");
-        if (!jdaLib.exists()) {
-            String build = bot.getJdaVersion().split("_")[1];
-            String url = "https://ci.dv8tion.net/job/JDA/" + build + "/artifact/build/libs/JDA-" + bot.getJdaVersion() + "-withDependencies-no-opus.jar";
-            jdaLib = Library.download(url, jdaLib.getName());
-        }
-        assert jdaLib != null;
-        Library.add(jdaLib);
     }
 
 }
