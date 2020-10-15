056bc2dc5362e0ac803a2a3f0e13ff8c36a7a6af
zp4rker <acc@zp4rker.com>
2020-10-15 22:18:21 +1100

    started filling in setup process

--- src/main/kotlin/com/zp4rker/disbot/Main.kt
+++ src/main/kotlin/com/zp4rker/disbot/Main.kt
@@ -1,6 +1,14 @@
 package com.zp4rker.disbot
 
+import com.github.jezza.TomlTable
+import com.zp4rker.disbot.config.TomlFile
 import com.zp4rker.disbot.console.Console
+import com.zp4rker.disbot.module.ModuleLoader
+import net.dv8tion.jda.api.JDABuilder
+import net.dv8tion.jda.api.entities.Activity
+import net.dv8tion.jda.api.hooks.AnnotatedEventManager
+import net.dv8tion.jda.api.requests.GatewayIntent
+import net.dv8tion.jda.api.utils.cache.CacheFlag
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import java.io.File
@@ -14,6 +22,9 @@ class Main {
 
     companion object {
         val logger: Logger = LoggerFactory.getLogger("Disbot")
+        private val modLoader: ModuleLoader = ModuleLoader()
+
+        private val config = TomlFile("bot.toml", Main::class.java.getResource("/bot.toml").readText())
 
         @JvmStatic fun main(args: Array<String>) {
             val inst = Main()
@@ -21,10 +32,27 @@ class Main {
 
             logger.info("Setting up...")
             // add setup code here
-                // load modules
-                // connect to jda
+            modLoader.loadModules() // load modules
+            JDABuilder.createDefault(config.get<String>("token"), GatewayIntent.values().toList()).build() // connect to jda
                 // post jda-connect events
             logger.info("Setup complete!")
+        }
+
+        private fun buildJDA() {
+            with(JDABuilder.createDefault(config.get<String>("token"), GatewayIntent.values().toList())) {
+                if (config.get<Boolean>("cacheEnabled") != false) {
+                    enableCache(CacheFlag.values().toList())
+                }
+                setEventManager(AnnotatedEventManager())
+                config.get<TomlTable>("activity")?.also {
+                    val type = Activity.ActivityType.valueOf(config.get<String>("activity.type")!!)
+                    val message = config.get<String>("message")!!
+                    val url = config.get<String>("url")!!
+
+                    if (type == Activity.ActivityType.STREAMING) setActivity(Activity.streaming(message, url))
+                    else setActivity(Activity.of(type, message))
+                }
+            }
         }
     }
 
