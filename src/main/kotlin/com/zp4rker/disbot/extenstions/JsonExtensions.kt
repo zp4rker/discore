369d2f0d9fff79ba2a50021ac9e8e67fb4d1d88c
zp4rker <acc@zp4rker.com>
2020-11-24 06:36:09 +1100

    added json extension

---
+++ src/main/kotlin/com/zp4rker/disbot/extenstions/JsonExtensions.kt
@@ -0,0 +1,20 @@
+package com.zp4rker.disbot.extenstions
+
+import org.json.JSONObject
+
+/**
+ * @author zp4rker
+ */
+
+fun JSONObject.getComplex(key: String): Any? {
+    val components = key.split(":")
+    var currentObj = this
+    var lastKey = components[0]
+    for (component in components) {
+        lastKey = component
+        if (currentObj.has(component) && currentObj[component] is JSONObject) {
+            currentObj = currentObj.getJSONObject(component)
+        }
+    }
+    return if (currentObj.has(lastKey)) currentObj[lastKey] else null
+}
