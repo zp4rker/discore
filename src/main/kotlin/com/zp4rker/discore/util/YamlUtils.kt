d6c157b85cd666d220c5aacca4d49dd71031db2f
zp4rker <acc@zp4rker.com>
2021-01-09 09:37:30 +1100

    fixed bug in loadYamlOrDefault

--- src/main/kotlin/com/zp4rker/discore/util/YamlUtils.kt
+++ src/main/kotlin/com/zp4rker/discore/util/YamlUtils.kt
@@ -5,6 +5,7 @@ import com.zp4rker.discore.Bot
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.encodeToString
 import java.io.File
+import kotlin.reflect.full.*
 
 /**
  * @author zp4rker
@@ -19,9 +20,12 @@ inline fun <reified T> loadYamlOrDefault(file: File): T {
 
     if (!file.exists()) file.createNewFile()
 
-    with(Bot::class.java.getResource("/${file.name}")) {
-        if (!File(toURI()).exists()) return T::class.java.getConstructor().newInstance().also { file.writeText(Yaml.default.encode(it)) }
-        file.writeText(readText())
+    with(Bot::class.java.getResourceAsStream("/${file.name}")) {
+        if (this == null) {
+            val default = T::class.companionObject!!.declaredMemberProperties.find { it.name == "default" }!!.getter.call(T::class.companionObjectInstance)
+            return (default as T).also { file.writeText(Yaml.default.encode(it)) }
+        }
+        file.writeText(use { it.reader().use { r -> r.readText() } })
         return Yaml.default.decodeFromString(file.readText())
     }
 }
