72d059e316be7e78572f081d75c14f85680ef846
zp4rker <acc@zp4rker.com>
2021-02-14 11:57:46 +1100

    added awaitreactions to messagechannel

--- src/main/kotlin/com/zp4rker/discore/extenstions/MessageChannelExtensions.kt
+++ src/main/kotlin/com/zp4rker/discore/extenstions/MessageChannelExtensions.kt
@@ -4,7 +4,10 @@ import com.zp4rker.discore.extenstions.event.Predicate
 import com.zp4rker.discore.extenstions.event.expectBlocking
 import net.dv8tion.jda.api.entities.Message
 import net.dv8tion.jda.api.entities.MessageChannel
+import net.dv8tion.jda.api.entities.MessageReaction
+import net.dv8tion.jda.api.entities.SelfUser
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent
+import net.dv8tion.jda.api.events.message.react.MessageReactionAddEvent
 
 /**
  * @author zp4rker
@@ -13,8 +16,18 @@ import net.dv8tion.jda.api.events.message.MessageReceivedEvent
 fun MessageChannel.awaitMessages(filter: Predicate<Message> = { true }, limit: Int = 1): List<Message> {
     val list = mutableListOf<Message>()
     while (list.size < limit) {
-        this.expectBlocking<MessageReceivedEvent>(amount = limit) {
-            if (filter(it.message)) list.add(it.message)
+        this.expectBlocking<MessageReceivedEvent> {
+            if (filter(it.message) && it.author !is SelfUser) list.add(it.message)
+        }
+    }
+    return list
+}
+
+fun MessageChannel.awaitReactions(filter: Predicate<MessageReaction> = { true }, limit: Int = 1): List<MessageReaction> {
+    val list = mutableListOf<MessageReaction>()
+    while (list.size < limit) {
+        this.expectBlocking<MessageReactionAddEvent> {
+            if (filter(it.reaction) && it.user !is SelfUser) list.add(it.reaction)
         }
     }
     return list
