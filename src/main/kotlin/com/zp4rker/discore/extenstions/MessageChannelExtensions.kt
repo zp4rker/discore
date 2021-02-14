43d509ab11b10c907255ac1211356fccb3281683
zp4rker <acc@zp4rker.com>
2021-02-14 11:48:45 +1100

    removed timeout from awaitmessages + added filter

--- src/main/kotlin/com/zp4rker/discore/extenstions/MessageChannelExtensions.kt
+++ src/main/kotlin/com/zp4rker/discore/extenstions/MessageChannelExtensions.kt
@@ -1,19 +1,21 @@
 package com.zp4rker.discore.extenstions
 
+import com.zp4rker.discore.extenstions.event.Predicate
 import com.zp4rker.discore.extenstions.event.expectBlocking
 import net.dv8tion.jda.api.entities.Message
 import net.dv8tion.jda.api.entities.MessageChannel
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent
-import java.util.concurrent.TimeUnit
 
 /**
  * @author zp4rker
  */
 
-fun MessageChannel.awaitMessages(limit: Int = 1, timeout: Long = 0, timeoutUnit: TimeUnit = TimeUnit.MILLISECONDS): List<Message> {
+fun MessageChannel.awaitMessages(filter: Predicate<Message> = { true }, limit: Int = 1): List<Message> {
     val list = mutableListOf<Message>()
-    this.expectBlocking<MessageReceivedEvent>(amount = limit, timeout = timeout, timeoutUnit = timeoutUnit) {
-        list.add(it.message)
+    while (list.size < limit) {
+        this.expectBlocking<MessageReceivedEvent>(amount = limit) {
+            if (filter(it.message)) list.add(it.message)
+        }
     }
     return list
 }
