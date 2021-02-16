355504970a9e4dbcb3cd9adb3e0ea3a5cc7ca6c7
zp4rker <acc@zp4rker.com>
2021-02-16 23:12:08 +1100

    added some guild channel extensions

---
+++ src/main/kotlin/com/zp4rker/discore/extenstions/GuildChannelExtensions.kt
@@ -0,0 +1,16 @@
+package com.zp4rker.discore.extenstions
+
+import net.dv8tion.jda.api.entities.Category
+import net.dv8tion.jda.api.entities.GuildChannel
+import net.dv8tion.jda.api.entities.TextChannel
+
+/**
+ * @author zp4rker
+ */
+// General
+fun GuildChannel.setName(name: String) = manager.setName(name)
+fun GuildChannel.setParent(parent: Category?) = manager.setParent(parent)
+fun GuildChannel.setPosition(position: Int) = manager.setPosition(position)
+
+// Text Channels
+fun TextChannel.setTopic(topic: String?) = manager.setTopic(topic)
